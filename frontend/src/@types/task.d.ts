037b089fc9d801ca0c370149657017f176f52d56
Joe McMahon <jmcmahon443@gmail.com>
2023-11-21 22:51:45 -0800

    update task data type

--- frontend/src/@types/task.d.ts
+++ frontend/src/@types/task.d.ts
@@ -1,16 +1,7 @@
-export interface ITaskQuery {
-  id: string;
-  robotid: string;
-  description: string;
-  completed: boolean;
-  created_at: string;
-}
-
 export interface ITask {
   id: string;
   robotid: string;
   description: string;
-  completed: boolean;
-  created_at: string;
   state: string;
+  created_at: string;
 }
